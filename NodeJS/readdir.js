a0809b0b9dc1e7541a81ca5b894179a2f151661c
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2020-10-20 16:50:44 +0900

    -
    -

--- NodeJS/readdir.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-fs.readdir("./data/", (err, files) => {
-  console.log(files);
-  files.forEach((value, index, array) => {
-    console.log(array[index]);
-    console.log(value);
-  });
-});
